71cb58d62c1d97d7910324e874debf87c53c084c
Justin McIntyre <justinmcintyre42@gmail.com>
2020-10-08 20:12:56 -0300

    we are currently capable of dynamicly adding content, I'm now just trying to add content from a file

---
+++ provinces/albertaContent.js
@@ -0,0 +1,6 @@
+//content goes in the backticks, don't use backticks in your content. and name the const after the province
+export default function(){}
+const alberta = 
+`<div>this is alberta's Content</div>`;
+
+return alberta;
--- scripts/provincePageContentScript.js
+++ scripts/provincePageContentScript.js
@@ -1,12 +1,9 @@
 //this script serves up the appropriate content depending on which province the user decides to browse
 
 //finds all the tags I'd want to change with this script
-let dynamicElementsp = [];
+let dynamicElement;
 function findAllTags (){
-    dynamicElementsp[0] = document.getElementById("provinceName");
-    dynamicElementsp[1] = document.getElementById("expensiveRestaurants");
-    dynamicElementsp[2] = document.getElementById("midrangeRestaurants");
-    dynamicElementsp[3] = document.getElementById("cheapRestaurants");
+    dynamicElement = document.getElementById("dynamicContent");
 }
 
 //this function will be triggered onclick --- it takes the elements id as param to know what content to serve
@@ -14,14 +11,28 @@ function newContentOnClick(event) {
     event = event || window.event; //IE
     let target = event.target || event.srcElement; //IE
     let id = target.id;
-    alert("you clicked on " + id);
+    dynamicElement.innerHTML = returnContent(`../provinces/albertaContent.html`, 'alberta');//still just a test, I need to pass a html from another document
+}
+
+//this map contains all the loaded content for the different provinces
+let loadedProvinces = {};
+//this function checks if the content has been previously imported and if not call the fetch function - it then returns the content
+function returnContent(path, key){
+    if(loadedProvinces[key] == undefined){
+        return fetchContentFromFile(path, key);
+    }else{
+        return loadedProvinces[key];
+    }
+}
+//this function returns the content of a specified file *Note this fuction will have to be changed to support a framwork like node.js when deployed on a server*
+function fetchContentFromFile(path, constName){
+    loadedProvinces[constName] = content;
+    return loadedProvinces[constName];
 }
 
 //finds all links that will change content and adds an onclick event to them
 function addOnclickForEach(item, index, arr){
-    item.setAttribute("onclick","newContentOnClick(event)");
-    //_*_*_*_*_*_*__*_ this next line is just to make sure that everything works
-    console.log(item.id);
+    item.setAttribute("onclick","newContentOnClick(event); return false;");//returns flase to stop the page from refreshing
 }
 let contentLinks = [];
 function findAllLinks() {
