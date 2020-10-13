51f71911142414c87487b8c22de3871cfdd7d0ac
Justin McIntyre <justinmcintyre42@gmail.com>
2020-10-13 13:15:29 -0300

    added a url praser to the province page prototype

--- scripts/provincePageContentScript.js
+++ scripts/provincePageContentScript.js
@@ -1,5 +1,27 @@
-//this script serves up the appropriate content depending on which province the user decides to browse
+//this functions allows this script to parse the url for variables
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
 
+//this script serves up the appropriate content depending on which province the user decides to browse
+function changeContent(province){
+    switch(province){
+        case 'Alberta': dynamicElement.innerHTML = albertaContent;
+        break;
+        case 'British Columbia': dynamicElement.innerHTML = british_columbiaContent;
+        break;
+        case 'Saskatchewan': dynamicElement.innerHTML = saskatchewanContent;
+        break;
+        default: dynamicElement.innerHTML = "<h2>Oops</h2>";
+    }
+}
 //finds all the tags I'd want to change with this script
 let dynamicElement;
 function findAllTags (){
@@ -11,16 +33,7 @@ function newContentOnClick(event) {
     event = event || window.event; //IE
     let target = event.target || event.srcElement; //IE
     let id = target.id;
-
-    switch(id){
-        case 'Alberta': dynamicElement.innerHTML = albertaContent;
-        break;
-        case 'British Columbia': dynamicElement.innerHTML = british_columbiaContent;
-        break;
-        case 'Saskatchewan': dynamicElement.innerHTML = saskatchewanContent;
-        break;
-        default: dynamicElement.innerHTML = "<h2>Oops</h2>";
-    }
+    changeContent(id);
 }
 
 //finds all links that will change content and adds an onclick event to them
@@ -38,4 +51,6 @@ function findAllLinks() {
 window.onload = function() {
     findAllTags();
     findAllLinks();
+    let currentProvince = getQueryVariable("province");
+    changeContent(currentProvince);
 };
