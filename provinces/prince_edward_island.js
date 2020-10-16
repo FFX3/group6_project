974dc1cf26aac6fef1e2dbd045dde3ef8ba0c85e
sharda00 <71936507+sharda00@users.noreply.github.com>
2020-10-16 02:42:32 -0400

    Added PEI,New Brunswick and Quebec JS

--- provinces/prince_edward_island.js
+++ provinces/prince_edward_island.js
@@ -1,4 +1,13 @@
 var prince_edward_islandContent =
 `
-<h2>You clicked on Prince Edward Island</h2><!-- remove this and add your content-->
+<h2>Restaurants worth a look in Charlottetown, Prince Edward Island</h2>
+<div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/JohnBrownRichmondStreetGrille.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/JohnBrownRichmondStreetGrille.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/JohnBrownRichmondStreetGrilleTN.jpg"><h3>John Brown Richmond Street Grille</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/PhoHungVietnameseRestaurant.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/PhoHungVietnameseRestaurant.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/PhoHungVietnameseRestaurantTN.jpg"><h3>Pho Hung Vietnamese Restaurant</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/PiattoPizzeria.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/PiattoPizzeria.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/PiattoPizzeriaTN.jpg"><h3>Piatto Pizzeria</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/TheBrickhouseKitchen&Bar.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/TheBrickhouseKitchen&Bar.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/TheBrickhouseKitchen&BarTN.jpg"><h3>The Brickhouse Kitchen & Bar</h3></div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/TheCork&Cast.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/TheCork&Cast.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/TheCork&CastTN.jpg"><h3>The Cork & Cast</h3></div>
+    <div class="restaurantContainer"><a href="./resources/charlottetown-Prince-Edward-Island/TheGahanHouse.jpg"><a href="./resources/charlottetown-Prince-Edward-Island/TheGahanHouse.jpg"><img src="./resources/charlottetown-Prince-Edward-Island/TheGahanHouseTN.jpg"><h3>The Gahan House</h3></div>
+</div>
+
 `;
--- provinces/quebec.js
+++ provinces/quebec.js
@@ -1,4 +1,13 @@
 var quebecContent =
 `
-<h2>You clicked on Quebec</h2><!-- remove this and add your content-->
+<h2>Restaurants worth a look in Quebec City, Quebec</h2>
+<div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/BachirRestaurant.jpg"><a href="./resources/quebec-city-Quebec/BachirRestaurant.jpg"><img src="./resources/quebec-city-Quebec/BachirRestaurantTN.jpg"><h3>Bachir Restaurant</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/BistroSousLeFort.jpg"><a href="./resources/quebec-city-Quebec/BistroSousLeFort.jpg"><img src="./resources/quebec-city-Quebec/BistroSousLeFortTN.jpg"><h3>Bistro Sous Le Fort</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/ChezBoulayBistroBoreal.jpg"><a href="./resources/quebec-city-Quebec/ChezBoulayBistroBoreal.jpg"><img src="./resources/quebec-city-Quebec/ChezBoulayBistroBorealTN.jpg"><h3>Chez Boulay Bistro Boreal</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/ChezRioux&Pettigrew.jpg"><a href="./resources/quebec-city-Quebec/ChezRioux&Pettigrew.jpg"><img src="./resources/quebec-city-Quebec/ChezRioux&PettigrewTN.jpg"><h3>Chez Rioux & Pettigrew</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/LegendeRestaurantBoreal.jpg"><a href="./resources/quebec-city-Quebec/LegendeRestaurantBoreal.jpg"><img src="./resources/quebec-city-Quebec/LegendeRestaurantBorealTN.jpg"><h3>Legende Restaurant Boreal</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/quebec-city-Quebec/Poutineville.jpg"><a href="./resources/quebec-city-Quebec/Poutineville.jpg"><img src="./resources/quebec-city-Quebec/PoutinevilleTN.jpg"><h3>PoutinevilleT</h3></a></a></div>
+</div>
+
 `;
