f16c5d7ec9e2554dce3108beec47a4ed23d2d445
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2020-10-15 20:46:12 -0400

    added alberta and nova scotia pictures

--- provinces/alberta.js
+++ provinces/alberta.js
@@ -1,4 +1,10 @@
-var albertaContent =
+var albertaContent = `
+<h2>Restaurants worth a look in Alberta</h2>
+<div>
+    <div class="restaurantContainer"><a href="./resources/Alberta/MochaCabanaTN.jpg"><a href="./resources/Alberta/MochaCabana.jpg"><img src="./resources/Alberta/MochaCabanaTN.jpg"><h3>Mocha Cabana</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Alberta/NativeTonguesTN.jpg"><a href="./resources/Alberta/NativeTongues.jpg"><img src="./resources/Alberta/NativeTonguesTN.jpg"><h3>Native Tongues</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Alberta/ss106yycTN.jpg"><a href="./resources/Alberta/ss106yyc.jpg"><img src="./resources/Alberta/ss106yycTN.jpg"><h3>SS106 Aperitivo Bar</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Alberta/tgaapTN.jpg"><a href="./resources/Alberta/tgaap.jpg"><img src="./resources/Alberta/tgaapTN.jpg"><h3>Two Guys and a Pizza</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Alberta/VeroBistroTN.jpg"><a href="./resources/Alberta/VeroBistro.jpg"><img src="./resources/Alberta/VeroBistroTN.jpg"><h3>Vero Bistro</h3></div></a></div>
+</div>
 `
-<h2>You clicked on Alberta</h2>
-`;
--- provinces/nova_scotia.js
+++ provinces/nova_scotia.js
@@ -1,4 +1,10 @@
-var nova_scotiaContent =
+var nova_scotiaContent = `
+<h2>Restaurants worth a look in Nova Scotia</h2>
+<div>
+    <div class="restaurantContainer"><a href="./resources/Nova Scotia/2DoorsDownTN.jpg"><a href="./resources/Nova Scotia/2DoorsDown.jpg"><img src="./resources/Nova Scotia/2DoorsDownTN.jpg"><h3>2 Doors Down Food and Wine</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Nova Scotia/GrandPreTN.jpg"><a href="./resources/Nova Scotia/GrandPre.jpg"><img src="./resources/Nova Scotia/GrandPreTN.jpg"><h3>Grand Pre Winery</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Nova Scotia/OldTriangleTN.jpg"><a href="./resources/Nova Scotia/OldTriangle.jpg"><img src="./resources/Nova Scotia/OldTriangleTN.jpg"><h3>The Old Triangle Irish Alehouse</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Nova Scotia/RhubarbTN.jpg"><a href="./resources/Nova Scotia/Rhubarb.jpg"><img src="./resources/Nova Scotia/RhubarbTN.jpg"><h3>Rhubarb</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/Nova Scotia/WildCarawayTN.jpg"><a href="./resources/Nova Scotia/WildCaraway.jpg"><img src="./resources/Nova Scotia/WildCarawayTN.jpg"><h3>Wild Caraway</h3></div></a></div>
+</div>
 `
-<h2>You clicked on Nova Scotia</h2><!-- remove this and add your content-->
-`;
