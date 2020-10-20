4b13fdfc81c6a6f04adb8c407b230d66cea548e7
Justin McIntyre <justinmcintyre42@gmail.com>
2020-10-20 15:57:18 -0300

    fixed a bug caused by inconsitent province variable formating, (I put 1 to many divs)

--- provinces/saskatchewan.js
+++ provinces/saskatchewan.js
@@ -1,7 +1,7 @@
 var saskatchewanContent = `
 <h2>Restaurants worth a look in Saskatchewan</h2>
 <div>
-    <div class="restaurantContainer"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><img src="./resources/saskatchewan/littleRedMarketCafeTN.jpg"><h3>Little Red Market Cafe</h3></div></a></div>
+    <div class="restaurantContainer"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><img src="./resources/saskatchewan/littleRedMarketCafeTN.jpg"><h3>Little Red Market Cafe</h3></a></div>
     <div class="restaurantContainer"><img src="./resources/saskatchewan/harvestEateryTN.jpg"><h3>Harvest Eatery</h3></div>
     <div class="restaurantContainer"><img src="./resources/saskatchewan/smittysFamilyTN.jpg"><h3>Smitty's Family Restaurant</h3></div>
     <div class="restaurantContainer"><img src="./resources/saskatchewan/mangoChefTN.jpg"><h3>Mango Chef Asian Fusion</h3></div>
