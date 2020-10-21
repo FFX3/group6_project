ea67b7188dd831f873255590eae5c9ac7d07c7d8
Justin McIntyre <justinmcintyre42@gmail.com>
2020-10-21 10:38:30 -0300

    finished saskatchewan

--- provinces/new_brunswick.js
+++ provinces/new_brunswick.js
@@ -2,11 +2,11 @@ var new_brunswickContent =
 `
 <h2>Restaurants worth a look in Fredericton, New Brunswick</h2>
 <div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/11thMile.jpg"><a href="./resources/fredericton-New-Brunswick/11thMile.jpg"><img src="./resources/fredericton-New-Brunswick/11thMileTN.jpg"><h3>11th Mile</h3></a></a></div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurant.jpg"><a href="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurant.jpg"><img src="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurantTN.jpg"><h3>Caribbean Flavas Restaurant</h3></a></a></div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/CinnamonCafe.jpg"><a href="./resources/fredericton-New-Brunswick/CinnamonCafe.jpg"><img src="./resources/fredericton-New-Brunswick/CinnamonCafeTN.jpg"><h3>Cinnamon Cafe</h3></a></a></div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavern.jpg"><a href="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavern.jpg"><img src="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavernTN.jpg"><h3>Sam Snead's Oak Grill and Tavern</h3></a>,/a></div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/TheMidsea.jpg"><a href="./resources/fredericton-New-Brunswick/TheMidsea.jpg"><img src="./resources/fredericton-New-Brunswick/TheMidseaTN.jpg"><h3>The Midsea</h3></a></a></div>
-    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/WolastoqWharf.jpg"><a href="./resources/fredericton-New-Brunswick/WolastoqWharf.jpg"><img src="./resources/fredericton-New-Brunswick/WolastoqWharfTN.jpg"><h3>Wolastoq Wharf</h3></a></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/11thMile.jpg"><a href="./resources/fredericton-New-Brunswick/11thMile.jpg"><img src="./resources/fredericton-New-Brunswick/11thMileTN.jpg"><h3>11th Mile</h3></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurant.jpg"><a href="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurant.jpg"><img src="./resources/fredericton-New-Brunswick/CaribbeanFlavasRestaurantTN.jpg"><h3>Caribbean Flavas Restaurant</h3></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/CinnamonCafe.jpg"><a href="./resources/fredericton-New-Brunswick/CinnamonCafe.jpg"><img src="./resources/fredericton-New-Brunswick/CinnamonCafeTN.jpg"><h3>Cinnamon Cafe</h3></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavern.jpg"><a href="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavern.jpg"><img src="./resources/fredericton-New-Brunswick/SamSnead'sOakGrillandTavernTN.jpg"><h3>Sam Snead's Oak Grill and Tavern</h3></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/TheMidsea.jpg"><a href="./resources/fredericton-New-Brunswick/TheMidsea.jpg"><img src="./resources/fredericton-New-Brunswick/TheMidseaTN.jpg"><h3>The Midsea</h3></a></div>
+    <div class="restaurantContainer"><a href="./resources/fredericton-New-Brunswick/WolastoqWharf.jpg"><a href="./resources/fredericton-New-Brunswick/WolastoqWharf.jpg"><img src="./resources/fredericton-New-Brunswick/WolastoqWharfTN.jpg"><h3>Wolastoq Wharf</h3></a></div>
 </div>
 `;
--- provinces/saskatchewan.js
+++ provinces/saskatchewan.js
@@ -1,10 +1,40 @@
 var saskatchewanContent = `
 <h2>Restaurants worth a look in Saskatchewan</h2>
 <div>
-    <div class="restaurantContainer"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><a href="./resources/saskatchewan/littleRedMarketCafe.jpg"><img src="./resources/saskatchewan/littleRedMarketCafeTN.jpg"><h3>Little Red Market Cafe</h3></a></div>
-    <div class="restaurantContainer"><img src="./resources/saskatchewan/harvestEateryTN.jpg"><h3>Harvest Eatery</h3></div>
-    <div class="restaurantContainer"><img src="./resources/saskatchewan/smittysFamilyTN.jpg"><h3>Smitty's Family Restaurant</h3></div>
-    <div class="restaurantContainer"><img src="./resources/saskatchewan/mangoChefTN.jpg"><h3>Mango Chef Asian Fusion</h3></div>
-    <div class="restaurantContainer"><img src="./resources/saskatchewan/theGrottoTN.jpg"><h3>The Grotto</h3></div>
+    <div class="restaurantContainer">
+        <a href="./resources/saskatchewan/littleRedMarketCafe.jpg">
+            <img src="./resources/saskatchewan/littleRedMarketCafeTN.jpg">
+            <h3>Little Red Market Cafe</h3>
+        </a>
+        <a href="https://www.facebook.com/LittleRedMarketCafe/" target="_BLANK">I want to eat here!</a>
+    </div>
+    <div class="restaurantContainer">
+        <a href="./resources/saskatchewan/harvestEatery.jpg">
+            <img src="./resources/saskatchewan/harvestEateryTN.jpg">
+            <h3>Harvest Eatery</h3>
+        </a>
+        <a href="https://www.eatharvest.ca/" target="_BLANK">I want to eat here!</a>
+    </div>
+    <div class="restaurantContainer">
+        <a href="./resources/saskatchewan/smittysFamily.jpg">
+            <img src="./resources/saskatchewan/smittysFamilyTN.jpg">
+            <h3>Smitty's Family Restaurant</h3>
+        </a>
+        <a href="https://www.facebook.com/SmittysMM/" target="_BLANK">I want to eat here!</a>
+    </div>
+    <div class="restaurantContainer">
+        <a href="./resources/saskatchewan/mangoChef.jpg">
+            <img src="./resources/saskatchewan/mangoChefTN.jpg">
+            <h3>Mango Chef Asian Fusion</h3>
+        </a>
+        <a href="https://mango-chef-asian-fusion-restaurant-restaurant.business.site/" target="_BLANK">I want to eat here!</a>
+    </div>
+    <div class="restaurantContainer">
+        <a href="./resources/saskatchewan/theGrotto.jpg">
+            <img src="./resources/saskatchewan/theGrottoTN.jpg">
+            <h3>The Grotto</h3>
+        </a>
+        <a href="https://www.facebook.com/The-Grotto-252748928079371/" target="_BLANK">I want to eat here!</a>
+    </div>
 </div>
 `
